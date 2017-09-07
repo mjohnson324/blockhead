010d7c0353ccfa24fc7cbcf58c7682b4cf04afa0
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-07 11:27:39 -0400

    Update bundle file

--- JavaScript/bundle.js
+++ JavaScript/bundle.js
@@ -65,8 +65,148 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const Game = __webpack_require__(1);
+const Display = __webpack_require__(5);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const blockheadBoard = document.getElementById("blockhead");
+  const ctx = blockheadBoard.getContext("2d");
+
+  const game = new Game(ctx);
+  new Display(game, ctx).start();
+});
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const Floor = __webpack_require__(2);
+const tutorial = __webpack_require__(4);
+
+class Game {
+  constructor(ctx) {
+    this.tutorial = tutorial;
+    this.ctx = ctx;
+  }
+
+  draw() {
+    const ctx = this.ctx;
+    const floor = new Floor(tutorial, ctx);
+  }
+}
+
+module.exports = Game;
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const Tile = __webpack_require__(3);
+
+class Floor {
+  constructor(positions, ctx) {
+    this.positions = positions;
+    this.ctx = ctx;
+
+    this.layTiles();
+  }
+
+  layTiles() {
+    const ctx = this.ctx;
+    this.positions.forEach(pos => {
+      const options = { position: pos, isGoal: false, isStart: false };
+      const tile = new Tile(ctx, options);
+      tile.draw();
+    });
+  }
+}
+
+module.exports = Floor;
+
+
+/***/ }),
+/* 3 */
 /***/ (function(module, exports) {
 
+class Tile {
+  constructor(ctx, options) {
+    this.ctx = ctx;
+    this.position = options.position;
+    this.isGoal = options.isGoal;
+    this.isStart = options.isStart;
+  }
+
+  draw() {
+    const { x, y } = this.position;
+    const ctx = this.ctx;
+    ctx.fillStyle = 'rgb(0, 255, 0)';
+    ctx.fillRect(x, y, 20, 20);
+    ctx.strokeRect(x, y, 20, 20);
+  }
+}
+
+module.exports = Tile;
+
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports) {
+
+const tutorial = [
+  { x: 500, y: 200 },
+  { x: 500, y: 220 },
+  { x: 500, y: 240 },
+  { x: 500, y: 260 },
+  { x: 500, y: 280 },
+
+  { x: 520, y: 200 },
+  { x: 520, y: 220 },
+  { x: 520, y: 240 },
+  { x: 520, y: 260 },
+  { x: 520, y: 280 },
+
+  { x: 540, y: 200 },
+  { x: 540, y: 220 },
+  { x: 540, y: 240 },
+  { x: 540, y: 260 },
+  { x: 540, y: 280 },
+
+  { x: 560, y: 200 },
+  { x: 560, y: 220 },
+  { x: 560, y: 240 },
+  { x: 560, y: 260 },
+  { x: 560, y: 280 },
+
+  { x: 580, y: 200 },
+  { x: 580, y: 220 },
+  { x: 580, y: 240 },
+  { x: 580, y: 260 },
+  { x: 580, y: 280 },
+];
+
+module.exports = tutorial;
+
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports) {
+
+class Display {
+  constructor(game, ctx) {
+    this.ctx = ctx;
+    this.game = game;
+  }
+
+  start() {
+    this.game.draw();
+  }
+}
+
+module.exports = Display;
 
 
 /***/ })
