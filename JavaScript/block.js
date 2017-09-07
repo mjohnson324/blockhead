d272d1bde176cef095db5f2098be47065d57621b
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-07 19:18:39 -0400

    Animate block prism movements

--- JavaScript/block.js
+++ JavaScript/block.js
@@ -2,20 +2,59 @@ class Block {
   constructor(ctx, startPos) {
     this.position = startPos;
     this.ctx = ctx;
+    this.dimensions = { width: 30, height: 30 };
   }
 
   move(i, j) {
-    const { x, y } = this.position;
-    this.position.x += i;
-    this.position.y += j;
+    const { dx, dy } = this.checkStanding(i, j);
+    this.position.x += dx;
+    this.position.y += dy;
     this.draw();
+  }
+
+  checkStanding(i, j) {
+    if (this.width === this.height) {
+      if (i > 0) {
+        this.width = 60;
+        return { dx: i, dy: j };
+      } else if ( i < 0) {
+        this.width = 60;
+        return { dx: 2 * i, dy: j };
+      } else if (j > 0) {
+        this.height = 60;
+        return { dx: i, dy: j };
+      } else {
+        this.height = 60;
+        return { dx: i, dy: 2 * j };
+      }
+    } else if (this.width > this.height) {
+      if (i > 0) {
+        this.width = 30;
+        return { dx: 2 * i, dy: j };
+      } else if ( i < 0) {
+        this.width = 30;
+        return { dx: i, dy: j };
+      } else {
+        return { dx: i, dy: j };
+      }
+    } else {
+      if (j > 0) {
+        this.height = 30;
+        return { dx: i, dy: 2 * j };
+      } else if (j < 0) {
+        this.height = 30;
+        return { dx: i, dy: j };
+      } else {
+        return { dx: i, dy: j };
+      }
+    }
   }
 
   draw() {
     const { x, y } = this.position;
     this.ctx.fillStyle = 'rgb(75, 0, 130)';
-    this.ctx.fillRect(x, y, 30, 30);
-    this.ctx.strokeRect(x, y, 30, 30);
+    this.ctx.fillRect(x, y, this.width, this.height);
+    this.ctx.strokeRect(x, y, this.width, this.height);
   }
 }
 
