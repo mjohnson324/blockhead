5fdd2913040c6f631fb207aef86b23a90ec52222
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-07 20:25:47 -0400

    Debug block rendering

--- JavaScript/block.js
+++ JavaScript/block.js
@@ -13,36 +13,36 @@ class Block {
   }
 
   checkStanding(i, j) {
-    if (this.width === this.height) {
+    if (this.dimensions.width === this.dimensions.height) {
       if (i > 0) {
-        this.width = 60;
+        this.dimensions.width = 60;
         return { dx: i, dy: j };
       } else if ( i < 0) {
-        this.width = 60;
+        this.dimensions.width = 60;
         return { dx: 2 * i, dy: j };
       } else if (j > 0) {
-        this.height = 60;
+        this.dimensions.height = 60;
         return { dx: i, dy: j };
       } else {
-        this.height = 60;
+        this.dimensions.height = 60;
         return { dx: i, dy: 2 * j };
       }
-    } else if (this.width > this.height) {
+    } else if (this.dimensions.width > this.dimensions.height) {
       if (i > 0) {
-        this.width = 30;
+        this.dimensions.width = 30;
         return { dx: 2 * i, dy: j };
       } else if ( i < 0) {
-        this.width = 30;
+        this.dimensions.width = 30;
         return { dx: i, dy: j };
       } else {
         return { dx: i, dy: j };
       }
     } else {
       if (j > 0) {
-        this.height = 30;
+        this.dimensions.height = 30;
         return { dx: i, dy: 2 * j };
       } else if (j < 0) {
-        this.height = 30;
+        this.dimensions.height = 30;
         return { dx: i, dy: j };
       } else {
         return { dx: i, dy: j };
@@ -52,9 +52,10 @@ class Block {
 
   draw() {
     const { x, y } = this.position;
-    this.ctx.fillStyle = 'rgb(75, 0, 130)';
-    this.ctx.fillRect(x, y, this.width, this.height);
-    this.ctx.strokeRect(x, y, this.width, this.height);
+    const { width, height } = this.dimensions;
+    this.ctx.fillStyle = 'rgb(200, 0, 255)';
+    this.ctx.fillRect(x, y, width, height);
+    this.ctx.strokeRect(x, y, width, height);
   }
 }
 
