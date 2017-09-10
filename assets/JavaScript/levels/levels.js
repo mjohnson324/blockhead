a877316c13dab29b74d0263e41675f1b8a64bd5d
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-10 16:24:29 -0400

    Add level one

--- assets/JavaScript/levels/levels.js
+++ assets/JavaScript/levels/levels.js
@@ -1,9 +1,15 @@
 const Tutorial = require('./tutorial');
+const LevelOne = require('./level_one');
+const LevelTwo = require('./level_two');
+const LevelThree = require('./level_three');
 
 const levels = (tileSize) => {
   return(
     [
-      Tutorial(tileSize, 360, 180)
+      Tutorial(tileSize, 360, 180),
+      LevelOne(tileSize, 330, 210),
+      LevelTwo(tileSize, 270, 270),
+      LevelThree(tileSize, 210, 190)
     ]);
 };
 
