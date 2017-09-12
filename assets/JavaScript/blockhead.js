4146012238ef46b89a43cdb642754beed9553aa1
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-12 19:40:04 -0400

    Update entry file, set game class as master class

--- assets/JavaScript/blockhead.js
+++ assets/JavaScript/blockhead.js
@@ -1,12 +1,9 @@
 const Game = require('./game');
-const Display = require('./display');
 
 document.addEventListener("DOMContentLoaded", () => {
   const blockheadBoard = document.getElementById("blockhead");
   const ctx = blockheadBoard.getContext("2d");
   blockheadBoard.width = 900;
   blockheadBoard.height = 500;
-
-  const game = new Game(ctx, 30);
-  new Display(game, ctx).start();
+  new Game(ctx, 30).start();
 });
