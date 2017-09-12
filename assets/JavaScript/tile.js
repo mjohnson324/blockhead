508668df0f4cbfcc385edf53e0e593d03b619af8
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-12 17:23:26 -0400

    Track tile properties in tile object

--- assets/JavaScript/tile.js
+++ assets/JavaScript/tile.js
@@ -1,29 +1,8 @@
 class Tile {
-  constructor(ctx, options) {
-    this.ctx = ctx;
+  constructor(options) {
     this.position = options.position;
     this.isGoal = options.isGoal;
     this.isStart = options.isStart;
-    this.size = options.size;
-  }
-
-  draw() {
-    const { x, y } = this.position;
-    const ctx = this.ctx;
-    const size = this.size;
-    ctx.fillStyle = this.statusCheck();
-    ctx.fillRect(x, y, size, size);
-    ctx.strokeRect(x, y, size, size);
-  }
-
-  statusCheck() {
-    if (this.isStart) {
-      return 'rgb(0, 255, 255)';
-    } else if (this.isGoal) {
-      return 'rgb(0, 255, 0)';
-    } else {
-      return 'rgb(192, 192, 192)';
-    }
   }
 }
 
