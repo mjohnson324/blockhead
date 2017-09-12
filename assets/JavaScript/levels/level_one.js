3f684d5c81bd5663363fa2c2e5d8dbbbf6d8ad9f
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-12 16:40:04 -0400

    Change variable name to length; export function invoked with game dimension

--- assets/JavaScript/levels/level_one.js
+++ assets/JavaScript/levels/level_one.js
@@ -1,51 +1,51 @@
-const levelOne = (size, startX, startY) => {
+const levelOne = (length, startX, startY) => {
   return(
     [
       { x: startX, y: startY, isStart: true },
-      { x: startX + size * 6, y: startY + size * 3, isGoal: true },
+      { x: startX + length * 6, y: startY + length * 3, isGoal: true },
 
-      { x: startX - size, y: startY - size },
-      { x: startX - size, y: startY },
-      { x: startX - size, y: startY + size },
+      { x: startX - length, y: startY - length },
+      { x: startX - length, y: startY },
+      { x: startX - length, y: startY + length },
 
-      { x: startX, y: startY - size },
-      { x: startX, y: startY + size },
-      { x: startX, y: startY + size * 2 },
+      { x: startX, y: startY - length },
+      { x: startX, y: startY + length },
+      { x: startX, y: startY + length * 2 },
 
-      { x: startX + size, y: startY - size },
-      { x: startX + size, y: startY },
-      { x: startX + size, y: startY + size },
-      { x: startX + size, y: startY + size * 2 },
+      { x: startX + length, y: startY - length },
+      { x: startX + length, y: startY },
+      { x: startX + length, y: startY + length },
+      { x: startX + length, y: startY + length * 2 },
 
-      { x: startX + size * 2, y: startY },
-      { x: startX + size * 2, y: startY + size },
-      { x: startX + size * 2, y: startY + size * 2 },
+      { x: startX + length * 2, y: startY },
+      { x: startX + length * 2, y: startY + length },
+      { x: startX + length * 2, y: startY + length * 2 },
 
-      { x: startX + size * 3, y: startY },
-      { x: startX + size * 3, y: startY + size },
-      { x: startX + size * 3, y: startY + size * 2 },
+      { x: startX + length * 3, y: startY },
+      { x: startX + length * 3, y: startY + length },
+      { x: startX + length * 3, y: startY + length * 2 },
 
-      { x: startX + size * 4, y: startY },
-      { x: startX + size * 4, y: startY + size },
-      { x: startX + size * 4, y: startY + size * 2 },
-      { x: startX + size * 4, y: startY + size * 3 },
+      { x: startX + length * 4, y: startY },
+      { x: startX + length * 4, y: startY + length },
+      { x: startX + length * 4, y: startY + length * 2 },
+      { x: startX + length * 4, y: startY + length * 3 },
 
-      { x: startX + size * 5, y: startY + size },
-      { x: startX + size * 5, y: startY + size * 2 },
-      { x: startX + size * 5, y: startY + size * 3 },
-      { x: startX + size * 5, y: startY + size * 4 },
+      { x: startX + length * 5, y: startY + length },
+      { x: startX + length * 5, y: startY + length * 2 },
+      { x: startX + length * 5, y: startY + length * 3 },
+      { x: startX + length * 5, y: startY + length * 4 },
 
-      { x: startX + size * 6, y: startY + size },
-      { x: startX + size * 6, y: startY + size * 2 },
-      { x: startX + size * 6, y: startY + size * 4 },
+      { x: startX + length * 6, y: startY + length },
+      { x: startX + length * 6, y: startY + length * 2 },
+      { x: startX + length * 6, y: startY + length * 4 },
 
-      { x: startX + size * 7, y: startY + size },
-      { x: startX + size * 7, y: startY + size * 2 },
-      { x: startX + size * 7, y: startY + size * 3 },
-      { x: startX + size * 7, y: startY + size * 4 },
+      { x: startX + length * 7, y: startY + length },
+      { x: startX + length * 7, y: startY + length * 2 },
+      { x: startX + length * 7, y: startY + length * 3 },
+      { x: startX + length * 7, y: startY + length * 4 },
 
-      { x: startX + size * 8, y: startY + size * 2 },
-      { x: startX + size * 8, y: startY + size * 3 },
+      { x: startX + length * 8, y: startY + length * 2 },
+      { x: startX + length * 8, y: startY + length * 3 },
     ]
   );
 };
--- assets/JavaScript/levels/levels.js
+++ assets/JavaScript/levels/levels.js
@@ -3,14 +3,15 @@ const LevelOne = require('./level_one');
 const LevelTwo = require('./level_two');
 const LevelThree = require('./level_three');
 
-const levels = (tileSize) => {
+const levels = (length) => {
   return(
     [
-      Tutorial(tileSize, 360, 180),
-      LevelOne(tileSize, 330, 240),
-      LevelTwo(tileSize, 270, 210),
-      LevelThree(tileSize, 210, 280)
+      length,
+      Tutorial(length, 360, 180),
+      LevelOne(length, 330, 240),
+      LevelTwo(length, 270, 210),
+      LevelThree(length, 210, 280)
     ]);
 };
 
-module.exports = levels;
+module.exports = levels(30);
