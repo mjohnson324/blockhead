8691a3676dc179d0d0818691baffde5d70dcbbe0
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-12 18:58:47 -0400

    Record tile color as property

--- assets/JavaScript/tile.js
+++ assets/JavaScript/tile.js
@@ -3,6 +3,17 @@ class Tile {
     this.position = options.position;
     this.isGoal = options.isGoal;
     this.isStart = options.isStart;
+    this.color = this.statusCheck();
+  }
+
+  statusCheck() {
+    if (this.isStart) {
+      return 'rgb(0, 255, 255)';
+    } else if (this.isGoal) {
+      return 'rgb(0, 255, 0)';
+    } else {
+      return 'rgb(192, 192, 192)';
+    }
   }
 }
 
