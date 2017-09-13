528b460d4b8cea8ad694050cae908f168a0f1488
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-13 11:04:13 -0400

    Write out all rendering functions: floor, block, failure, and finish

--- assets/JavaScript/display.js
+++ assets/JavaScript/display.js
@@ -1,42 +1,76 @@
-const Tile = require("./tile");
-const Levels = require("./levels/levels");
-
 class Display {
-  constructor(game, ctx) {
+  constructor(ctx, length) {
     this.ctx = ctx;
-    this.game = game;
+    this.length = length;
+    const backgroundRGB = [25, 25, 25];
+    this.backgroundColor = this.stringifyRGB(backgroundRGB);
   }
 
-  start() {
-    this.game.draw();
-    this.game.handleBoard();
+  stringifyRGB(colorArray) {
+    return(
+      'rgb('
+      .concat(colorArray[0])
+      .concat(', ')
+      .concat(colorArray[1])
+      .concat(', ')
+      .concat(colorArray[2])
+      .concat(')')
+    );
+  }
+
+  render(floor, block) {
+    this.ctx.fillStyle = this.backgroundColor;
+    this.ctx.fillRect(0, 0, 900, 500);
+    this.ctx.font = '30px sans-serif';
+    this.ctx.fillStyle = 'white';
+    this.ctx.fillText(`Level ${this.levelNumber}`, 25, 50);
+    this.drawFloor(floor);
+    this.drawBlock(block);
+  }
+
+  drawFloor(floor) {
+    floor.forEach(tile => {
+      this.ctx.fillStyle = tile.color;
+      const { xPos, yPos } = tile;
+      this.ctx.fillRect(xPos, yPos, this.length, this.length);
+      this.ctx.strokeRect(xPos, yPos, this.length, this.length);
+    });
+  }
+
+  drawBlock(block) {
+    const { xPos, yPos, width, height } = block;
+    this.ctx.fillStyle = 'rgb(200, 0, 255)';
+    this.ctx.fillRect(xPos, yPos, width, height);
+    this.ctx.strokeRect(xPos, yPos, width, height);
+  }
+
+  tileMovesOffFloor(coordinates) {
+    for(let i = 0; i < coordinates,length; i++) {
+      let corner = coordinates[i];
+      let point = this.ctx.getImageData(corner[0], corner[1], 1, 1);
+      let colorData = point.data.slice(0, 3);
+      let color = this.stringifyRGB(colorData);
+      if (color === this.backgroundColor) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  drawFail(oldOptions) {
+    const { xPos, yPos, width, height } = oldOptions;
+    this.ctx.fillStyle = 'rgb(255, 0, 0)';
+    this.ctx.fillRect(xPos, yPos, width, height);
+  }
+
+  drawFinish() {
+    this.ctx.clearRect(0, 0, 900, 500);
+    this.ctx.font = '20px sans-serif';
+    this.ctx.fillText(
+      "Thanks for playing! More levels coming soon! (probably)",
+      50,
+      300);
   }
 }
 
 module.exports = Display;
-
-
-
-class Floor {
-  constructor(positions, ctx) {
-    this.positions = positions;
-    this.ctx = ctx;
-    this.tileSize = tileSize;
-  }
-
-  layTiles() {
-    const ctx = this.ctx;
-    this.positions.forEach(pos => {
-      const options = {
-        size: this.tileSize,
-        position: { x: pos.x, y: pos.y },
-        isGoal: pos.isGoal,
-        isStart: pos.isStart
-      };
-      const tile = new Tile(ctx, options);
-      tile.draw();
-    });
-  }
-}
-
-module.exports = Floor;
