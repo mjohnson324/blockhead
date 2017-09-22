45e6ce4bf67502b668f9b7776c3d9f1c95ddc9d4
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-22 13:31:57 -0400

    Resolve path issues with audio by renaming files

--- assets/JavaScript/sound.js
+++ assets/JavaScript/sound.js
@@ -1,9 +1,9 @@
 class Sound {
   constructor(musicButton) {
-    this.rectangleSound = new Audio('../Sounds/Effects/270315__littlerobotsoundfactory__menu-navigate-03.wav');
-    this.squareSound = new Audio('../Sounds/Effects/270324__littlerobotsoundfactory__menu-navigate-00.wav');
-    this.fall = new Audio('../Sounds/Effects/270311__littlerobotsoundfactory__explosion-03.wav');
-    this.music = new Audio('../Sounds/Music/toby fox - UNDERTALE Soundtrack - 09 Enemy Approaching.mp3');
+    this.rectangleSound = new Audio('../Sounds/Effects/lrsf-menu-nav-3-rec.wav');
+    this.squareSound = new Audio('../Sounds/Effects/lrsf-menu-nav-0-square.wav');
+    this.fall = new Audio('../Sounds/Effects/lrsf-explode-3-fall.wav');
+    this.music = new Audio('../Sounds/Music/undertale-09.mp3');
     this.musicButton = musicButton;
     this.playMusic = true;
     this.music.loop = true;
