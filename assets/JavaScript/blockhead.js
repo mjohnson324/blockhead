4bbb573ba11c573c183d1e5f765e1e3cc44b3fc1
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-22 14:37:56 -0400

    add button to toggle display of directions

--- assets/JavaScript/blockhead.js
+++ assets/JavaScript/blockhead.js
@@ -1,9 +1,23 @@
 const Game = require('./game');
 
 document.addEventListener("DOMContentLoaded", () => {
+  const directions = document.getElementById('directions');
+  const directionsDisplay = document.getElementById('direct');
+  directionsDisplay.addEventListener("click", () => {
+    toggleView(directions);
+  });
+
   const blockheadBoard = document.getElementById("blockhead");
   const ctx = blockheadBoard.getContext("2d");
   blockheadBoard.width = 900;
   blockheadBoard.height = 500;
   new Game(ctx, 30).start();
 });
+
+function toggleView(directions) {
+  if (directions.className === "hidden") {
+    directions.className = "display";
+  } else {
+    directions.className = "hidden";
+  }
+}
