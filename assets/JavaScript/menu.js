c9d22752e20336126704e5835ef84034d79cc169
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-01-08 20:51:56 -0500

    Add menu options to start game or tutorial level, and example code for click-based option event listeners

--- assets/JavaScript/menu.js
+++ assets/JavaScript/menu.js
@@ -1,9 +1,31 @@
 class Menu {
-  constructor() {
+  constructor(canvas) {
     this.textSize = "medium";
     this.blockSize = "medium";
     this.lives = false;
     this.challenge = false;
+
+    this.activateMenu(canvas);
+  }
+
+  activateMenu(canvas) {
+    // elem.addEventListener('click', function(e) {
+    //     console.log('click: ' + e.offsetX + '/' + e.offsetY);
+    //     var rect = collides(rects, e.offsetX, e.offsetY);
+    //     if (rect) {
+    //         console.log('collision: ' + rect.x + '/' + rect.y);
+    //     } else {
+    //         console.log('no collision');
+    //     }
+    // }, false);
+  }
+
+  startGame() {
+
+  }
+
+  beginTutorial() {
+
   }
 
   accessOptions() {
@@ -23,7 +45,7 @@ class Menu {
   }
 
   enableChallengeMode() {
-    
+
   }
 }
 
