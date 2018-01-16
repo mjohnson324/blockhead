b70fc549c70687b2529675b2bd117f499b22aaf6
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-01-15 23:51:49 -0500

    Initialize testing for LevelGenerator object

--- assets/JavaScript/test/level_generator.test.js
+++ assets/JavaScript/test/level_generator.test.js
@@ -1,1 +1,7 @@
 const LevelGenerator = require('../level_generator');
+
+const levels = LevelGenerator(30);
+
+test('Initializes LevelGenerator', () => {
+  expect(levels).toBeInstanceOf(LevelGenerator);
+});
