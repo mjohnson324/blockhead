9d5814ac7a08c3525e683bb5fee7b4c7a044baba
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-01-15 22:24:42 -0500

    Initialize new Block for every test

--- assets/JavaScript/test/block.test.js
+++ assets/JavaScript/test/block.test.js
@@ -1,5 +1,11 @@
 const Block = require('../block');
 
+let block;
+
+beforeEach(() => {
+  block = new Block({ xPos : 0, yPos: 0, width: 30, height: 30 });
+})
+
 test('block initializes with options', () => {
-  expect(new Block({})).toBeInstanceOf(Block);
+  expect(block).toBeInstanceOf(Block);
 });
