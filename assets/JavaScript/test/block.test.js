43e30aa4a2675883a299824a387f2120e2080017
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-01-22 21:26:18 -0500

    Outline structure of test suite for player block class

--- assets/JavaScript/test/block.test.js
+++ assets/JavaScript/test/block.test.js
@@ -1,17 +1,69 @@
 const Block = require('../block');
 
-let block;
 
-beforeEach(() => {
-  block = new Block({ xPos : 0, yPos: 0, width: 30, height: 30 });
+describe('Square blocks becoming rectangular', () => {
+  let block;
+  beforeEach(() => {
+    block = new Block({ xPos: 0, yPos: 0, width: 30, height: 30 });
+  });
+
+  test('block changes position incrementally', () => {
+    block.changePosition(30, 30);
+    expect(block.xPos).toBe(30);
+    expect(block.yPos).toBe(30);
+  });
+
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
 })
 
-test('block initializes with options', () => {
-  expect(block).toBeInstanceOf(Block);
-});
+describe('Wide rectangular block', () => {
+  let wideBlock;
+  beforeEach(() => {
+    wideBlock = new Block({xPos: 0, yPos: 0, width: 60, height: 30 });
+  });
 
-test('block changes position incrementally', () => {
-  block.changePosition(30, 30);
-  expect(block.xPos).toBe(30);
-  expect(block.yPos).toBe(30);
-});
+  test('Wide block retains shape when moving up and down', () => {
+
+  });
+
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
+})
+
+describe('Tall Rec', () => {
+  let tallBlock;
+  beforeEach(() => {
+    tallBlock = new Block({xPos: 0, yPos: 0, width: 60, height: 30 });
+  });
+
+  test('Tall block retains shape when moving left and right', () => {
+
+  });
+
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
+})
