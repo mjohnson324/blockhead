711db42271cffbe6d1865d0c77464700e6162fb4
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-01-23 17:36:26 -0500

    Refactor buttons: eliminate redundant code

--- assets/JavaScript/page_buttons.js
+++ assets/JavaScript/page_buttons.js
@@ -1,94 +1,38 @@
 class PageButtons {
   constructor() {
-    this.activateButtons();
+    this.toggleImage = this.toggleImage.bind(this);
+
+    this.activateTutorialImages();
+    this.activateDirections();
   }
 
-  activateButtons() {
-    this.directionButtons();
-    this.fallButton();
-    this.transformButton();
-    this.goalButton();
-    this.moveButton();
-  }
-
-  directionButtons () {
-    const directionsButton = document.getElementById('direct-button');
-    const directionsDisplay = document.getElementById('direct');
-    directionsButton.addEventListener("click", () => {
-      this.toggleDirections(directionsDisplay);
+  activateTutorialImages() {
+    const imageIds = ['falling', 'goal', 'move', 'transform'];
+    imageIds.forEach(id => {
+      let button = document.getElementById(`${id}-button`);
+      let displayImage = document.getElementById(`${id}`);
+      button.addEventListener("click", () => {
+        this.toggleImage(displayImage);
+      });
     });
   }
 
-  fallButton() {
-    const fallButton = document.getElementById('falling-button');
-    const fallImage = document.getElementById('falling');
-    fallButton.addEventListener('click', () => {
-      this.fallToggle(fallImage);
+  activateDirections() {
+    const button = document.getElementById('direct-button');
+    const display = document.getElementById('direct');
+    button.addEventListener("click", () => {
+      this.toggleDirections(display);
     });
   }
 
-  goalButton() {
-    const goalButton = document.getElementById('goal-button');
-    const goalImage = document.getElementById('goal');
-    goalButton.addEventListener('click', () => {
-      this.goalToggle(goalImage);
-    });
-  }
-
-  moveButton() {
-    const moveButton = document.getElementById('move-button');
-    const moveImage = document.getElementById('move');
-    moveButton.addEventListener('click', () => {
-      this.moveToggle(moveImage);
-    });
-  }
-
-  transformButton() {
-    const transformButton = document.getElementById('transform-button');
-    const transformImage = document.getElementById('transform');
-    transformButton.addEventListener('click', () => {
-      this.transformToggle(transformImage);
-    });
+  toggleImage(image) {
+    image.className === "hidden" ?
+      image.className = "show" : image.className = "hidden";
   }
 
   toggleDirections(directions) {
-    if (directions.className === "hidden") {
-      directions.className = "display";
-    } else {
-      directions.className = "hidden";
-    }
-  }
-
-  moveToggle(image) {
-    if (image.className === "hidden") {
-      image.className = "show";
-    } else {
-      image.className = "hidden";
-    }
-  }
-
-  transformToggle(image) {
-    if (image.className === "hidden") {
-      image.className = "show";
-    } else {
-      image.className = "hidden";
-    }
-  }
-
-  fallToggle(image) {
-    if (image.className === "hidden") {
-      image.className = "show";
-    } else {
-      image.className = "hidden";
-    }
-  }
-
-  goalToggle(image) {
-    if (image.className === "hidden") {
-      image.className = "show";
-    } else {
-      image.className = "hidden";
-    }
+    directions.className === "hidden" ?
+      directions.className = "display" : directions.className = "hidden";
   }
 }
 
