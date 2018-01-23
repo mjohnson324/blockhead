eaf23a4a5e27c63c4d7d9375b71a37bfd1d06dd7
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-01-22 22:59:15 -0500

    Retrieve level information in aggregate export instead of importing individually

--- assets/JavaScript/level_generator.js
+++ assets/JavaScript/level_generator.js
@@ -1,18 +1,8 @@
 const Tile = require('./tile');
-
-const tutorial = require('./levels/tutorial');
-const levelOne = require('./levels/level_one');
-const levelTwo = require('./levels/level_two');
-const levelThree = require('./levels/level_three');
-const levelFour = require('./levels/level_four');
-
+const allLevels = require('./levels/all_levels');
 
 const levelGenerator = (length) => {
-  const levels = [tutorial(length),
-                     levelOne(length),
-                     levelTwo(length),
-                     levelThree(length),
-                     levelFour(length)];
+  const levels = allLevels.map(level => level(length));
   levels.forEach(level => {
     level.forEach((positionData, idx) => {
       level[idx] = new Tile(positionData);
---
+++ assets/JavaScript/levels/all_levels.js
@@ -0,0 +1,13 @@
+const tutorial = require('./tutorial');
+const levelOne = require('./level_one');
+const levelTwo = require('./level_two');
+const levelThree = require('./level_three');
+const levelFour = require('./level_four');
+
+module.exports = [
+  tutorial,
+  levelOne,
+  levelTwo,
+  levelThree,
+  levelFour
+];
