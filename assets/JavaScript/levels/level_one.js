12415835cfd00020a6c3585036a9b8861532d49b
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-01-28 01:07:36 -0500

    Update level data again to incorporate overall floor size for dynamic centering

--- assets/JavaScript/levels/level_one.js
+++ assets/JavaScript/levels/level_one.js
@@ -1,16 +1,17 @@
-const levelOne = [
-  { x: 0, y: 0, type: "start" },
-  { x: 9, y: 0, type: "goal" },
-  { x: 1, y: 0, type: "none" },
-  { x: 2, y: 0, type: "none" },
-  { x: 3, y: 0, type: "none" },
-  { x: 4, y: 0, type: "none" },
-  { x: 5, y: 0, type: "none" },
-  { x: 6, y: 0, type: "none" },
-  { x: 7, y: 0, type: "none" },
-  { x: 8, y: 0, type: "none" }
-];
-
-// { xRange: 10, yRange: 1, }
+const levelOne = {
+  floorDimensions: { xRange: 10, yRange: 1, },
+  tileData: [
+    { x: 0, y: 0, type: "start" },
+    { x: 9, y: 0, type: "goal" },
+    { x: 1, y: 0, type: "none" },
+    { x: 2, y: 0, type: "none" },
+    { x: 3, y: 0, type: "none" },
+    { x: 4, y: 0, type: "none" },
+    { x: 5, y: 0, type: "none" },
+    { x: 6, y: 0, type: "none" },
+    { x: 7, y: 0, type: "none" },
+    { x: 8, y: 0, type: "none" }
+  ],
+};
 
 module.exports = levelOne;
