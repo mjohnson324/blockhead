8c0391ca2207386177ba08332317d5952af5a40b
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-01-29 23:29:06 -0500

    Set up initial tests for tracking the current level

--- assets/JavaScript/test/level_generator.test.js
+++ assets/JavaScript/test/level_generator.test.js
@@ -1,11 +1,13 @@
 const LevelGenerator = require('../level_generator');
+const levels = require('../levels/all_levels');
 
-const levels = new LevelGenerator(30);
+const levels = new LevelGenerator(30, levels);
 
-test('Initializes LevelGenerator', () => {
-  expect(levels).toBeInstanceOf(LevelGenerator);
+test('LevelGenerator keeps track of the current level', () => {
+  expect(levels.currentLevel).toBe(1);
 });
 
-test('Keeps track of the current level', () => {
-  expect(levels.currentLevel).toBe(1);
+test('LevelGenerator progresses to the next level', () => {
+  levels.nextLevel();
+  expect(levels.currentLevel).toBe(2);
 });
