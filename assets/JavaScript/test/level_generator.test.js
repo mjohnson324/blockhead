b0280aebca9fc60cc43951de5841bc338f61d710
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-01-30 21:55:30 -0500

    Add levels sample for easy testing

--- assets/JavaScript/test/level_generator.test.js
+++ assets/JavaScript/test/level_generator.test.js
@@ -1,6 +1,24 @@
 const LevelGenerator = require('../level_generator');
-const levels = require('../levels/all_levels');
 
+const levels = {
+  1: {
+    floorDimensions: { xRange: , yRange: },
+    floorData: [
+      { x: 0, y: 0, type: "start" },
+      { x: 0, y: 2, type: "goal" },
+      { x: 0, y: 1, type: "none" },
+    ]
+  },
+  2: {
+    floorDimensions: { xRange: , yRange: },
+    floorData: [
+      { x: 0, y: 0, type: "start" },
+      { x: 2, y: 1, type: "goal" },
+      { x: 1, y: 0, type: "none" },
+      { x: 1, y: 1, type: "none" },
+    ]
+  },
+};
 const levels = new LevelGenerator(30, levels);
 
 test('LevelGenerator keeps track of the current level', () => {
