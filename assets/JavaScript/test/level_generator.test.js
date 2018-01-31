cbb383500e320fae1121fc9965b7fd9702f0b2c8
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-01-30 22:48:22 -0500

    Round out LevelGenerator tests by confirming the correct floor is made after progressing to the next level

--- assets/JavaScript/test/level_generator.test.js
+++ assets/JavaScript/test/level_generator.test.js
@@ -22,41 +22,56 @@ const levels = {
 };
 const levelMaker = new LevelGenerator(30, levels);
 
-test('LevelGenerator keeps track of the current level', () => {
-  expect(levelMaker.currentLevel).toBe(1);
+describe('Floor construction and helper functions', () => {
+  test('LevelGenerator keeps track of the current level', () => {
+    expect(levelMaker.currentLevel).toBe(1);
+  });
+
+  test('Dynamically create data for tiles', () => {
+    const start = { xPos: 100, yPos: 100 };
+    const tileInfo = { x: 3, y: 5, type: "none" };
+    const tileOptions = levelMaker.constructTileCoordinates(tileInfo, start);
+    expect(tileOptions).toEqual({ x: 190, y: 250, type: "none" });
+  });
+
+  test('Dynamically center floors', () => {
+    const dimensions = { xRange: 15, yRange: 15 };
+    const start = levelMaker.centerFloor(dimensions);
+    expect(start).toEqual({ xPos: 225, yPos: 25 });
+  });
+
+  test('Set up a floor of tiles for gameplay', () => {
+    const newLevel = {
+      '[435, 205]': new Tile({ x: 435, y: 205, type: "start" }),
+      '[435, 265]': new Tile({ x: 435, y: 265, type: "goal" }),
+      '[435, 235]': new Tile({ x: 435, y: 235, type: "none" }),
+    };
+    levelMaker.constructFloor();
+    expect(levelMaker.constructedFloor).toEqual(newLevel)
+  });
+
+  test('Tiles can be looked up individually', () => {
+    levelMaker.constructFloor();
+    const comparisonTile = new Tile({ x: 435, y: 235, type: "none" });
+    const actualTile = levelMaker.lookupTile({ xPos: 435, yPos: 235 });
+    expect(actualTile).toEqual(comparisonTile);
+  });
 });
 
-test('Dynamically create data for tiles', () => {
-  const start = { xPos: 100, yPos: 100 };
-  const tileInfo = { x: 3, y: 5, type: "none" };
-  const tileOptions = levelMaker.constructTileCoordinates(tileInfo, start);
-  expect(tileOptions).toEqual({ x: 190, y: 250, type: "none" });
-});
+describe('Floor construction as game progresses', () => {
+  test('LevelGenerator progresses to the next level', () => {
+    levelMaker.nextLevel();
+    expect(levelMaker.currentLevel).toBe(2);
+  });
 
-test('Dynamically center floors', () => {
-  const dimensions = { xRange: 15, yRange: 15 };
-  const start = levelMaker.centerFloor(dimensions);
-  expect(start).toEqual({ xPos: 225, yPos: 25 });
-});
-
-test('Set up a floor of tiles for gameplay', () => {
-  const newLevel = {
-    '[435, 205]': new Tile({ x: 435, y: 205, type: "start" }),
-    '[435, 265]': new Tile({ x: 435, y: 265, type: "goal" }),
-    '[435, 235]': new Tile({ x: 435, y: 235, type: "none" }),
-  };
-  levelMaker.constructFloor();
-  expect(levelMaker.constructedFloor).toEqual(newLevel)
-});
-
-test('Tiles can be looked up individually', () => {
-  levelMaker.constructFloor();
-  const comparisonTile = new Tile({ x: 435, y: 235, type: "none" });
-  const actualTile = levelMaker.lookupTile({ xPos: 435, yPos: 235 });
-  expect(actualTile).toEqual(comparisonTile);
-});
-
-test('LevelGenerator progresses to the next level', () => {
-  levelMaker.nextLevel();
-  expect(levelMaker.currentLevel).toBe(2);
+  test('LevelGenerator constructs the right floor after progressing', () => {
+    const testFloor = {
+      '[405, 220]': new Tile({ x: 405, y: 220, type: "start" }),
+      '[465, 250]': new Tile({ x: 465, y: 250, type: "goal" }),
+      '[435, 220]': new Tile({ x: 435, y: 220, type: "none" }),
+      '[435, 250]': new Tile({ x: 435, y: 250, type: "none" }),
+    };
+    levelMaker.constructFloor();
+    expect(levelMaker.constructedFloor).toEqual(testFloor);
+  });
 });
