9d63c22d5b0d30d2738cf12c10dd752c4574ebc4
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-02-01 18:52:11 -0500

    Test tile initialization using single-letter option types

--- assets/JavaScript/test/tile.test.js
+++ assets/JavaScript/test/tile.test.js
@@ -3,9 +3,20 @@ const Tile = require('../tile');
 let tile;
 
 beforeEach(() => {
-  tile = new Tile({ xPos: 0, yPos: 0, });
+  tile = new Tile({ x: 0, y: 0, type: "n" });
 });
 
 test('Tile initializes with options', () => {
-  expect(tile).toBeInstanceOf(Tile);
+  expect(tile.xPos).toEqual(0);
+  expect(tile.yPos).toEqual(0);
+  expect(tile.type).toEqual("none");
+});
+
+test('Tiles are assigned the correct type', () => {
+  const noType = tile.typeReference("n");
+  const startType = tile.typeReference("s");
+  const bridgeType = tile.typeReference("b");
+  expect(noType).toEqual("none");
+  expect(startType).toEqual("start");
+  expect(bridgeType).toEqual("bridge");
 });
