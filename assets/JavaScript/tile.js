20bd40ecca488b3a0cd2ff8c26f71f534ea9a53b
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-02-01 18:53:24 -0500

    Update Tile class to track information on tile types to avoid typo errors in levels

--- assets/JavaScript/tile.js
+++ assets/JavaScript/tile.js
@@ -2,7 +2,28 @@ class Tile {
   constructor(options) {
     this.xPos = options.x;
     this.yPos = options.y;
-    this.type = options.type;
+    this.type = this.typeReference(options.type);
+  }
+
+  typeReference(type) {
+    switch(type) {
+      case "n":
+        return "none";
+      case "s":
+        return "start";
+      case "g":
+        return "goal";
+      case "c":
+        return "collapsible";
+      case "w":
+        return "warp";
+      case "h":
+        return "heavySwitch";
+      case "l":
+        return "lightSwitch";
+      case "b":
+        return "bridge";
+    }
   }
 }
 
