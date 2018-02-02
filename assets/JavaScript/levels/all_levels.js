b444b9e032376369ac13f66d19ad286f661b1719
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-02-01 20:26:27 -0500

    Add level five to level roster

--- assets/JavaScript/levels/all_levels.js
+++ assets/JavaScript/levels/all_levels.js
@@ -3,6 +3,11 @@ const levelOne = require('./level_one');
 const levelTwo = require('./level_two');
 const levelThree = require('./level_three');
 const levelFour = require('./level_four');
+const levelFive = require('./level_five');
+const levelSix = require('./level_six');
+const levelSeven = require('./level_seven');
+const levelEight = require('./level_eight');
+const levelNine = require('./level_nine');
 
 module.exports = {
   tutorial: tutorial,
@@ -10,4 +15,10 @@ module.exports = {
   2: levelTwo,
   3: levelThree,
   4: levelFour,
+  5: levelFive,
 };
+
+// 6: levelSix,
+// 7: levelSeven,
+// 8: levelEight,
+// 9: levelNine,
