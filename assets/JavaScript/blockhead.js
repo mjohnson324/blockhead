eac5ed6deb9da2e68896572e29b1435cc6346efc
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2019-07-18 17:24:44 -0400

    Update page with new layout, images, and canvas that sizes to the current window

--- assets/JavaScript/blockhead.js
+++ assets/JavaScript/blockhead.js
@@ -2,8 +2,9 @@ const Game = require("./game");
 
 document.addEventListener("DOMContentLoaded", () => {
     const blockheadBoard = document.getElementById("blockhead");
+    const blockheadBoardContainer = document.getElementById("canvas-container");
     const ctx = blockheadBoard.getContext("2d");
-    blockheadBoard.width = 900;
-    blockheadBoard.height = 500;
+    blockheadBoard.width = blockheadBoardContainer.width;
+    blockheadBoard.height = blockheadBoardContainer.height;
     new Game(ctx, 30).start();
 });
