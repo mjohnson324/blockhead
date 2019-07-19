11a4161d2a3d8a1912f7cec8bba20e144c0fb9d6
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2019-07-19 19:11:10 -0400

    Refactor game class to remove unnecessary functions and attributes

--- assets/JavaScript/game.js
+++ assets/JavaScript/game.js
@@ -2,38 +2,40 @@ const Block = require("./game_objects/block");
 const controls = require("./game_objects/controls");
 const Display = require("./game_objects/display");
 const LevelGenerator = require("./game_objects/level_generator");
-// const Menu = require("./game_objects/menu");
+const Menu = require("./game_objects/menu");
 const sound = require("./game_objects/sound");
 
 const GameMusic = new sound.GameMusic();
 
 class Game {
-    constructor(ctx, length) {
-        this.display = new Display(ctx, length);
-        this.levels = new LevelGenerator(length);
-        this.block = new Block(length, { width: length, height: length });
-
+    constructor(ctx, length, boardSize) {
+        this.boardSize = boardSize;
+        this.length = length;
         this.pauseStatus = true;
         this.moves = 0;
         this.falls = 0;
+
+        this.display = new Display(ctx, length);
+        this.levels = new LevelGenerator(length);
+        this.block = new Block(length, length);
+        this.menu = new Menu(this.display);
 
         this.move = this.move.bind(this);
         this.pause = this.pause.bind(this);
         this.restart = this.restart.bind(this);
     }
 
-    start() {
+    // start() {
+    //     this.display.drawStart();
+    //     document.addEventListener("keydown", this.StartMenu);
+    // }
 
-    }
-
-    startMenu() {
-        GameMusic.startMenu();
-    }
+    // startMenu() {
+    //     GameMusic.startMenu();
+    // }
 
     startGame() {
         GameMusic.startGame();
-        this.moves = 0;
-        this.falls = 0;
         this.levels.constructFloor();
         this.block.setPosition(this.levels.currentStartPosition);
         this.timerId = setInterval(this.display.drawTime, 1000);
@@ -45,12 +47,14 @@ class Game {
 
     restart(e) {
         controls.restartGame(e, this);
+        this.moves = 0;
+        this.falls = 0;
     }
 
     move(e) {
         const arrowKeycodes = [37, 38, 39, 40];
         if (arrowKeycodes.includes(e.keyCode)) {
-            controls.getMove(e, this.block);
+            controls.getMove(e, this.block, this.length);
             this.moves += 1;
             this.checkBlock();
         }
@@ -80,7 +84,8 @@ class Game {
     }
 
     checkGoal() {
-        const tile = this.levels.lookupTile(this.block.position());
+        const { xPos, yPos } = this.block;
+        const tile = this.levels.lookupTile({ xPos, yPos });
         if (tile !== undefined && tile.type === "goal") {
             this.nextLevel();
         }
@@ -120,9 +125,10 @@ class Game {
         }
     }
 
+    // gets current position of block. Coordinates are set to avoid bugs related
+    // to narrow gaps.
     getCoordinates() {
-        const { xPos, yPos } = this.block.position();
-        const { width, height } = this.block.dimensions();
+        const { xPos, yPos, width, height } = this.block;
         return [
             [xPos + Math.floor(width / 4), yPos + Math.floor(height / 4)],
             [xPos + Math.floor(width * 3 / 4), yPos + Math.floor(height * 3 / 4)],
@@ -137,9 +143,9 @@ class Game {
 
     flashFailure() {
         this.display.render(this.displayOptions());
-        this.display.drawFail(this.block.properties());
+        this.display.drawFail(this.block);
         sound.playFallSound();
-        this.block.resetBlock(this.levels.currentStartPosition);
+        this.block.resetBlock(this.length, this.levels.currentStartPosition);
         setTimeout(() => {
             this.display.render(this.displayOptions());
             this.display.drawBlock(this.block);
