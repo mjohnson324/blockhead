07bc4c87f9963e2fc21486de044a6c053c40db6d
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2019-07-19 16:16:55 -0400

    Update styling of page

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  entry: "./assets/JavaScript/blockhead.js",
-  output: {
-    path: __dirname + "/assets/JavaScript/bundle",
-    filename: "bundle.js"
-	},
-	devtool: "source-maps",
-  resolve: {
-    extensions: [".js"]
-  }
+    entry: "./assets/JavaScript/blockhead.js",
+    output: {
+        path: __dirname + "/assets/JavaScript/bundle",
+        filename: "bundle.js"
+    },
+    devtool: "source-maps",
+    resolve: {
+        extensions: [".js"]
+    },
+    mode: "production",
 };
