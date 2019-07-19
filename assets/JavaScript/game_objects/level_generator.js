1b415bc4ac67b5b2a130e4e0eddf1d6e81b1e4fc
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2019-07-19 19:23:58 -0400

    Update level generator for dynamic rendering on mobile and desktop

--- assets/JavaScript/game_objects/level_generator.js
+++ assets/JavaScript/game_objects/level_generator.js
@@ -2,9 +2,8 @@ const Tile = require("./tile");
 const allLevels = require("../levels/all_levels");
 
 class LevelGenerator {
-    constructor(length) {
+    constructor() {
         this.currentLevel = 1;
-        this.length = length;
         this.levelData = allLevels;
 
         this.constructTileCoordinates = this.constructTileCoordinates.bind(this);
@@ -18,8 +17,8 @@ class LevelGenerator {
         this.currentLevel = 1;
     }
 
-    constructFloor() {
-        this.constructedFloor = this.generateLevel();
+    constructFloor(length) {
+        this.constructedFloor = this.generateLevel(length);
         this.currentStartPosition = this.getStart(this.constructedFloor);
     }
 
@@ -32,26 +31,26 @@ class LevelGenerator {
         }
     }
 
-    generateLevel() {
+    generateLevel(length) {
         const level = this.levelData[this.currentLevel];
-        const startPosition = this.centerFloor(level.floorDimensions);
-        return this.setCoordinates(level.floorData, startPosition);
+        const startPosition = this.centerFloor(level.floorDimensions, length);
+        return this.setCoordinates(level.floorData, startPosition, length);
     }
 
-    centerFloor(floorDimensions) { // Tiles are positioned relative to the
+    centerFloor(floorDimensions, length) { // Tiles are positioned relative to the
         const canvasWidth = 900;     // position of the top-left tile on a floor.
         const canvasHeight = 500;
-        const floorWidth = floorDimensions.xRange * this.length;
-        const floorHeight = floorDimensions.yRange * this.length;
+        const floorWidth = floorDimensions.xRange * length;
+        const floorHeight = floorDimensions.yRange * length;
         const startCornerXPos = Math.floor((canvasWidth - floorWidth) / 2);
         const startCornerYPos = Math.floor((canvasHeight - floorHeight) / 2);
         return { xPos: startCornerXPos, yPos: startCornerYPos };
     }
 
-    setCoordinates(floorData, startPosition) {
+    setCoordinates(floorData, startPosition, length) {
         const newFloor = {};
         floorData.forEach(tileData => {
-            let tileOptions = this.constructTileCoordinates(tileData, startPosition);
+            let tileOptions = this.constructTileCoordinates(tileData, startPosition, length);
             let tile = new Tile(tileOptions);
             let tilePosition = `[${tile.xPos}, ${tile.yPos}]`;
             newFloor[tilePosition] = tile;
@@ -59,9 +58,9 @@ class LevelGenerator {
         return newFloor;
     }
 
-    constructTileCoordinates(tileData, startPosition) {
-        const x = startPosition.xPos + this.length * tileData.x;
-        const y = startPosition.yPos + this.length * tileData.y;
+    constructTileCoordinates(tileData, startPosition, length) {
+        const x = startPosition.xPos + length * tileData.x;
+        const y = startPosition.yPos + length * tileData.y;
         return { x: x, y: y, type: tileData.type, active: tileData.active };
     }
 
